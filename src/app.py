f883dd0f1628242fc2e522042be5da987dcf68ec
Stratiev <vikcho.stratiev@abv.bg>
2026-01-03 14:30:07 +0200

    app.py reformatted

--- src/app.py
+++ src/app.py
@@ -14,7 +14,7 @@ GREETINGS = [
     "Bonjour le monde! (French)",
     "Hallo Welt! (German)",
     "Ciao Mondo! (Italian)",
-    "Zdravey, Svyat! (Bulgarian)"
+    "Zdravey, Svyat! (Bulgarian)",
 ]
 
 
